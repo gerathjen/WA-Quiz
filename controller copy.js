71e11381d6687929a1de8fedffb6266c5cc22b5b
Gerald Rathjen <gerald.rathjen@web.de>
2016-02-11 10:19:34 +0100

    Zweite Version mit drei Antworten

--- controller copy.js
+++ controller copy.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 	
 var questionNumber=0;
-// Anzahl Fragen
+// Wegen Array Wert um eins erhöht
+// Beispiel: bei Laden von fünf Fragen muss der Wert 6 angegeben werden
 var maximumQuestions=5;
 var questionBank=new Array();
 var stage="#game1";
@@ -18,6 +19,7 @@ for(i=0;i<data.quizlist.length;i++){
 	questionBank[i][1]=data.quizlist[i].option1;
 	questionBank[i][2]=data.quizlist[i].option2;
 	questionBank[i][3]=data.quizlist[i].option3;
+	questionBank[i][4]=data.quizlist[i].option4;
 }
 	numberOfQuestions=questionBank.length; 
 	
@@ -39,17 +41,19 @@ function scrambleDatabase(){
 
 function displayQuestion(){
 
- var rnd=Math.random()*3;
+ var rnd=Math.random()*4;
  rnd=Math.ceil(rnd);
  var q1;
  var q2;
  var q3;
+ var q4;
 
-if(rnd==1){q1=questionBank[questionNumber][1]; q2=questionBank[questionNumber][2]; q3=questionBank[questionNumber][3];}1
-if(rnd==2){q2=questionBank[questionNumber][1]; q3=questionBank[questionNumber][2]; q1=questionBank[questionNumber][3];}
-if(rnd==3){q3=questionBank[questionNumber][1];q1=questionBank[questionNumber][2];q2=questionBank[questionNumber][3];}
-    
-$(stage).append('<div class = "questionText">' + questionBank[questionNumber][0] + '</div><div id= "1" class="option">'+q1+'</div> <div id="2" class="option">'+q2+'</div> <div id="3" class="option">'+q3+'</div>');
+if(rnd==1){q1=questionBank[questionNumber][1];q2=questionBank[questionNumber][2];q3=questionBank[questionNumber][3];q4=questionBank[questionNumber][4];}
+if(rnd==2){q2=questionBank[questionNumber][1];q3=questionBank[questionNumber][2];q4=questionBank[questionNumber][4];q1=questionBank[questionNumber][3];}
+if(rnd==3){q3=questionBank[questionNumber][1];q1=questionBank[questionNumber][2];q2=questionBank[questionNumber][3];q4=questionBank[questionNumber][4];}
+if(rnd==4){q4=questionBank[questionNumber][1];q1=questionBank[questionNumber][2];q2=questionBank[questionNumber][3];q3=questionBank[questionNumber][3];}
+
+$(stage).append('<div class="questionText">'+questionBank[questionNumber][0]+'</div><div id="1" class="option">'+q1+'</div><div id="2" class="option">'+q2+'</div><div id="3" class="option">'+q3+'</div></div><div id="4" class="option">'+q4+'</div>');
 
 $('.option').click(function(){
   if(questionLock==false){questionLock=true;	
@@ -75,7 +79,7 @@ function changeQuestion(){
 		else{stage2="#game2";stage="#game1";}
 	
 	// Abfrage maximale Fragenanzahl
-	if(questionNumber<maximumQuestions){displayQuestion();}else{displayFinalSlide();}
+	if(questionNumber<numberOfQuestions && numberOfQuestions<maximumQuestions){displayQuestion();}else{displayFinalSlide();}
 	
 	$(stage2).animate({"right": "+=800px"},"slow", function() {$(stage2).css('right','-800px');$(stage2).empty();});
 	$(stage).animate({"right": "+=800px"},"slow", function() {questionLock=false;});
@@ -84,6 +88,10 @@ function changeQuestion(){
 
 function displayFinalSlide(){
 
+	// Button "Neu starten"	
+	// $(stage).append('<div class="questionText">Klasse, du hast das Quiz abgeschlossen!<br><br>Anzahl Fragen: '+numberOfQuestions+'<br>Richtige Antworten: '+score+'<br><input type="button" value="Jetzt neu starten?" id="b_neuladen" onclick="location.reload()"></div>');
+	// Wegen Array muss der Wert für die Ermittlung richtiger Anzahl um eins reduziert werden
+	maximumQuestions=maximumQuestions-1
 	$(stage).append('<div class="questionText">Klasse, du hast das Quiz abgeschlossen!<br><br>Anzahl Fragen: '+maximumQuestions+'<br>Richtige Antworten: '+score+'</div><div class="normalText"><img src="anderscool-jmd.png" alt="anders? - cool!-Logo" align="right" hspace="40">Serviceb&uuml;ro Jugendmigrationsdienste<br>Adenauerallee 12-14<br>53113 Bonn<br><br>t. 0228 / 95968-0<br>f. 0228 / 95968-30<br>anders-cool@jugendmigrationsdienste.de<br>www.anderscool.de</div>');
 	// Neustarten des Quiz nach 10 Sekunden //
 	setTimeout(function() { location.reload() },10000);
